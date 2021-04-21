7794d0fa1dc2a8ee110aa9d6d055554a53e608bc
Rao Akif <akifrao@gmail.com>
2021-04-21 14:41:40 +0500

    surahData.js Completed

--- src/components/SurahShowcase.js
+++ src/components/SurahShowcase.js
@@ -2,33 +2,29 @@ import { surahData } from '../surahData.js';
 import ReactPlayer from 'react-player/youtube';
 import { useParams } from 'react-router-dom';
 import '../App.css';
-import "./SurahShowcase.css"
+import './SurahShowcase.css';
 
 const Surah = () => {
   let { id } = useParams();
   let surah = surahData[id];
 
   return (
-    <div style={{margin: 'auto',
-      width: '50%',
-      padding: '10px'}}>
+    <div style={{ margin: 'auto', width: '50%', padding: '10px' }}>
       {/* <h2 style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}}>{surah.name}</h2> */}
       <ReactPlayer
-      className='react-player'
-      url={surah.link}
-      controls="true"
-      playing="false"
-      pip="true"
-      width='100%'
-      height='100%'
-      position= 'absolute'
-      top= '0'
-      left= '0'
+        className="react-player"
+        url={surah.link}
+        controls="true"
+        playing="false"
+        pip="true"
+        width="100%"
+        height="100%"
+        position="absolute"
+        top="0"
+        left="0"
       />
     </div>
   );
 };
 
 export default Surah;
-
-
--- src/surahData.js
+++ src/surahData.js
@@ -19,5 +19,555 @@ export const surahData = {
   "link":
   "https://www.youtube.com/watch?v=M5rEF6wPMto"
 },
+  "5": {
+  "name": "المائدہ",
+  "link":
+  "https://www.youtube.com/watch?v=tUbgbzlVLYk&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=5"
+},
+  "6": {
+  "name": "الانعام",
+  "link":
+  "https://www.youtube.com/watch?v=sOdZdWr6SQo&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=6"
+},
+  "7": {
+  "name": "الاعراف",
+  "link":
+  "https://www.youtube.com/watch?v=SYn1iSmg-b4&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=7"
+},
+  "8": {
+  "name": "الانفال",
+  "link":
+  "https://www.youtube.com/watch?v=pVgtoj68R8g&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=114"
+},
+  "9": {
+  "name": "	التوبہ",
+  "link":
+  "https://www.youtube.com/watch?v=sTJ1eQuClnU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=8"
+},
+  "10": {
+  "name": "یونس",
+  "link":
+  "https://www.youtube.com/watch?v=2k2LKr6mkMw&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=9"
+},
+  "11": {
+  "name": "ہود",
+  "link":
+  "https://www.youtube.com/watch?v=ymtYSnewsNI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=10"
+},
+  "12": {
+  "name": "یوسف",
+  "link":
+  "https://www.youtube.com/watch?v=0RVSHV2dEOA&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=11"
+},
+  "13": {
+  "name": "الرعد",
+  "link":
+  "https://www.youtube.com/watch?v=6TcAjpurDi8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=12"
+},
+  "14": {
+  "name": "ابراہیم",
+  "link":
+  "https://www.youtube.com/watch?v=NK93dirEvzg&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=13"
+},
+  "15": {
+  "name": "الحجر",
+  "link":
+  "https://www.youtube.com/watch?v=k7opUSjCSAM&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=14"
+},
+  "16": {
+  "name": "النحل",
+  "link":
+  "https://www.youtube.com/watch?v=9KRkZQFFVW4&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=15"
+},
+  "17": {
+  "name": "الاسراء",
+  "link":
+  "https://www.youtube.com/watch?v=JKFegOvHhqQ&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=16"
+},
+  "18": {
+  "name": "الکہف",
+  "link":
+  "https://www.youtube.com/watch?v=_Z130mLGckI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=17"
+},
+  "19": {
+  "name": "مریم",
+  "link":
+  "https://www.youtube.com/watch?v=Qpg3utW7luI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=18"
+},
+  "20": {
+  "name": "طٰہٰ",
+  "link":
+  "https://www.youtube.com/watch?v=dLQpw2xme70&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=19"
+},
+  "21": {
+  "name": "الانبیاء",
+  "link":
+  "https://www.youtube.com/watch?v=hFzmcwXtdw0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=20"
+},
+  "22": {
+  "name": "	الحج",
+  "link":
+  "https://www.youtube.com/watch?v=pul4zDEHDUA&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=21"
+},
+  "23": {
+  "name": "المؤمنون",
+  "link":
+  "https://www.youtube.com/watch?v=zVE4MEhrngQ&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=22"
+},
+  "24": {
+  "name": "	النور",
+  "link":
+  "https://www.youtube.com/watch?v=7QZjQuGjoKc&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=23"
+},
+  "25": {
+  "name": "الفرقان",
+  "link":
+  "https://www.youtube.com/watch?v=_weHW-9qX9Q&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=24"
+},
+  "26": {
+  "name": "الشعرآء",
+  "link":
+  "https://www.youtube.com/watch?v=S_7vTujrAt4&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=25"
+},
+  "27": {
+  "name": "	النمل",
+  "link":
+  "https://www.youtube.com/watch?v=modTxE8MesQ&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=26"
+},
+  "28": {
+  "name": "القصص",
+  "link":
+  "https://www.youtube.com/watch?v=17rfPp75sGE&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=27"
+},
+  "29": {
+  "name": "العنکبوت",
+  "link":
+  "https://www.youtube.com/watch?v=U7r7z4GP1ts&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=28"
+},
+  "30": {
+  "name": "الروم",
+  "link":
+  "https://www.youtube.com/watch?v=SeA9C8_Xo6I&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=29"
+},
+  "31": {
+  "name": "لقمان",
+  "link":
+  "https://www.youtube.com/watch?v=LfdAXC2dh9Y&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=30"
+},
+  "32": {
+  "name": "السجدہ",
+  "link":
+  "https://www.youtube.com/watch?v=wT-6D9ZzPb4&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=31"
+},
+  "33": {
+  "name": "الاحزاب",
+  "link":
+  "https://www.youtube.com/watch?v=x-AmDnnxy7E&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=32"
+},
+  "34": {
+  "name": "سبا",
+  "link":
+  "https://www.youtube.com/watch?v=PgTI5IcgQGg&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=33"
+},
+  "35": {
+  "name": "فاطر",
+  "link":
+  "https://www.youtube.com/watch?v=gNPo3Gr2c-A&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=34"
+},
+  "36": {
+  "name": "یس",
+  "link":
+  "https://www.youtube.com/watch?v=bATpN-tBxMI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=35"
+},
+  "37": {
+  "name": "الصافات",
+  "link":
+  "https://www.youtube.com/watch?v=vBG4_S9cIvU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=36"
+},
+  "38": {
+  "name": "ص",
+  "link":
+  "https://www.youtube.com/watch?v=pXC21jNRXpE&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=37"
+},
+  "39": {
+  "name": "الزمر",
+  "link":
+  "https://www.youtube.com/watch?v=dF5D_NZ-7jo&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=38"
+},
+  "40": {
+  "name": "المؤمن",
+  "link":
+  "https://www.youtube.com/watch?v=jJxGztc-hDU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=39"
+},
+  "41": {
+  "name": "حم السجدہ",
+  "link":
+  "https://www.youtube.com/watch?v=n4nFaCa7G7w&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=40"
+},
+  "42": {
+  "name": "الشوریٰ",
+  "link":
+  "https://www.youtube.com/watch?v=jHTAnb94Hjw&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=41"
+},
+  "43": {
+  "name": "الزخرف",
+  "link":
+  "https://www.youtube.com/watch?v=VU-9g1Ho2Fw&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=42"
+},
+  "44": {
+  "name": "الدخان",
+  "link":
+  "https://www.youtube.com/watch?v=il3DiJyP79s&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=43"
+},
+  "45": {
+  "name": "الجاثیہ",
+  "link":
+  "https://www.youtube.com/watch?v=qhUF8yisOYk&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=44"
+},
+  "46": {
+  "name": "الاحقاف",
+  "link":
+  "https://www.youtube.com/watch?v=qIcs2Zk4SDg&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=45"
+},
+  "47": {
+  "name": "محمد",
+  "link":
+  "https://www.youtube.com/watch?v=F_d7oCe00Bs&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=46"
+},
+  "48": {
+  "name": "الفتح",
+  "link":
+  "https://www.youtube.com/watch?v=qrj9Ktqh348&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=47"
+},
+  "49": {
+  "name": "الحجرات",
+  "link":
+  "https://www.youtube.com/watch?v=Fvlr_JgMFPo&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=48"
+},
+  "50": {
+  "name": "ق",
+  "link":
+  "https://www.youtube.com/watch?v=Xc27u4ITGI8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=49"
+},
+  "51": {
+  "name": "الذاریات",
+  "link":
+  "https://www.youtube.com/watch?v=zDVYP3EbLp0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=50"
+},
+  "52": {
+  "name": "سورۃ الطور",
+  "link":
+  "https://www.youtube.com/watch?v=Lo_TCZH60ac&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=51"
+},
+  "53": {
+  "name": "سورہ النجم",
+  "link":
+  "https://www.youtube.com/watch?v=Um-47-q10K0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=52"
+},
+  "54": {
+  "name": "سورہ القمر",
+  "link":
+  "https://www.youtube.com/watch?v=UjQUUm-aB-I&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=53"
+},
+  "55": {
+  "name": "سورہ الرحٰم",
+  "link":
+  "https://www.youtube.com/watch?v=jMCBmZmG4pI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=54"
+},
+  "56": {
+    "name": "سورہ الواقعہ",
+    "link":
+  "https://www.youtube.com/watch?v=ApC0ktu22dY&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=55"
+},
+  "57": {
+    "name": "سورہ الحدید",
+    "link":
+  "https://www.youtube.com/watch?v=krlkf36y_4U&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=56"
+},
+  "58": {
+    "name": "سورہ المجادلہ",
+    "link":
+  "https://www.youtube.com/watch?v=Uyy4X7fb2OI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=57"
+},
+  "59": {
+    "name": "سورہ الحشر",
+    "link":
+  "https://www.youtube.com/watch?v=XGfhaOr6-2s&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=58"
+},
+  "60": {
+  "name": "سورہ الممتحنہ",
+  "link":
+  "https://www.youtube.com/watch?v=OBcmKfFZRzU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=59"
+},
+  "61": {
+  "name": "سورہ الصف",
+  "link":
+  "https://www.youtube.com/watch?v=s1XSVJsvaP8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=60"
+},
+  "62": {
+  "name": "سورہ الجمعہ",
+  "link":
+  "https://www.youtube.com/watch?v=i7XlDfvbW2A&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=61"
+},
+  "63": {
+  "name": "سورہ المنافقون",
+  "link":
+  "https://www.youtube.com/watch?v=5G2qntKXrsU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=62"
+},
+  "64": {
+  "name": "سورہ التغابن",
+  "link":
+  "https://www.youtube.com/watch?v=QQuKNGjL-zg&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=63"
+},
+  "65": {
+  "name": "سورہ الطلاق",
+  "link":
+  "https://www.youtube.com/watch?v=VsTI_8jo-J8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=64"
+},
+  "66": {
+  "name": "سورہ التحریم",
+  "link":
+  "https://www.youtube.com/watch?v=Rd9RIDqDa28&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=65"
+},
+  "67": {
+  "name": "سورہ الملک",
+  "link":
+  "https://www.youtube.com/watch?v=aFQyaCn2Y_0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=66"
+},
+  "68": {
+  "name": "سورہ القلم",
+  "link":
+  "https://www.youtube.com/watch?v=Gor4AHCnMi0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=67"
+},
+  "69": {
+  "name": "سورہ الحاقہ",
+  "link":
+  "https://www.youtube.com/watch?v=YZfpW6JTM-E&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=68"
+},
+  "70": {
+  "name": "سورہ المعارج",
+  "link":
+  "https://www.youtube.com/watch?v=hlS3ftzD6eY&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=69"
+},
+  "71": {
+  "name": "نوح",
+  "link":
+  "https://www.youtube.com/watch?v=N_7z-uNfZYE&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=70"
+},
+  "72": {
+  "name": "سورہ الجن",
+  "link":
+  "https://www.youtube.com/watch?v=tavcrENtDUc&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=71"
+},
+  "73": {
+  "name": "سورہ المزمل",
+  "link":
+  "https://www.youtube.com/watch?v=cLNytPKcbLI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=72"
+},
+  "74": {
+  "name": "سورہ المدثر",
+  "link":
+  "https://www.youtube.com/watch?v=pmP6Q3StpV0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=73"
+},
+  "75": {
+  "name": "سورہ القیامہ",
+  "link":
+  "https://www.youtube.com/watch?v=VA7gjajuSxk&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=74"
+},
+  "76": {
+  "name": "سورہ الدہر",
+  "link":
+  "https://www.youtube.com/watch?v=KGk11DFxnr0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=75"
+},
+  "77": {
+  "name": "سورہ المرسلات",
+  "link":
+  "https://www.youtube.com/watch?v=QOTn689-stU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=76"
+},
+  "78": {
+  "name": "سورہ النباء",
+  "link":
+  "https://www.youtube.com/watch?v=dF4Otr73zO8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=77"
+},
+  "79": {
+  "name": "سورہ النازعات",
+  "link":
+  "https://www.youtube.com/watch?v=xOUxV5UqMEY&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=78"
+},
+  "80": {
+  "name": "سورہ عبس",
+  "link":
+  "https://www.youtube.com/watch?v=J1Xzz9NOhdM&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=79"
+},
+  "81": {
+  "name": "سورہ التکویر",
+  "link":
+  "https://www.youtube.com/watch?v=PSeo5z7AUlM&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=80"
+},
+  "82": {
+  "name": "سورہ الانفطار",
+  "link":
+  "https://www.youtube.com/watch?v=xNceGeizbV0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=81"
+},
+  "83": {
+  "name": "سورہ المطففین",
+  "link":
+  "https://www.youtube.com/watch?v=bZswxVG4qPs&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=82"
+},
+  "84": {
+  "name": "سورہ الانشقاق",
+  "link":
+  "https://www.youtube.com/watch?v=2tSgxJ8R1Fg&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=83"
+},
+  "85": {
+  "name": "سورہ البروج",
+  "link":
+  "https://www.youtube.com/watch?v=E2zj9OH0tgk&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=84"
+},
+  "86": {
+  "name": "سورہ الطارق",
+  "link":
+  "https://www.youtube.com/watch?v=bQN3rnA9GXk&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=85"
+},
+  "87": {
+  "name": "سورہ الاعلیٰ",
+  "link":
+  "https://www.youtube.com/watch?v=rEOZE86z9tw&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=86"
+},
+  "88": {
+  "name": "سورہ الغاشیہ",
+  "link":
+  "https://www.youtube.com/watch?v=AxcRQpX-yxs&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=87"
+},
+  "89": {
+  "name": "سورہ الفجر",
+  "link":
+  "https://www.youtube.com/watch?v=yaW3aNm8FO8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=88"
+},
+  "90": {
+  "name": "سورہ البلد",
+  "link":
+  "https://www.youtube.com/watch?v=o00gPte6eD0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=89"
+},
+  "91": {
+  "name": "سورہ الشمس",
+  "link":
+  "https://www.youtube.com/watch?v=cY-E6UbSrx8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=90"
+},
+  "92": {
+  "name": "سورہ اللیل",
+  "link":
+  "https://www.youtube.com/watch?v=sGmpfkcd3gI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=91"
+},
+  "93": {
+  "name": "سورہ الضحٰی",
+  "link":
+  "https://www.youtube.com/watch?v=B6sdjNc3UQU&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=92"
+},
+  "94": {
+  "name": "سورہ الم نشرح",
+  "link":
+  "https://www.youtube.com/watch?v=AHS9ZtQLejY&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=93"
+},
+  "95": {
+  "name": "سورہ التین",
+  "link":
+  "https://www.youtube.com/watch?v=7Aavq7a6r_8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=94"
+},
+  "96": {
+  "name": "سورہ العلق",
+  "link":
+  "https://www.youtube.com/watch?v=aqR9YD74WQI&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=95"
+},
+  "97": {
+  "name": "سورہ القدر",
+  "link":
+  "https://www.youtube.com/watch?v=vfhTXLNGL-U&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=96"
+},
+  "98": {
+  "name": "سورہ البینہ",
+  "link":
+  "https://www.youtube.com/watch?v=nDKpnkzmZdA&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=97"
+},
+  "99": {
+  "name": "سورہ الزلزال",
+  "link":
+  "https://www.youtube.com/watch?v=2Zfi_ZeP0Gc&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=98"
+},
+  "100": {
+  "name": "سورہ العادیات",
+  "link":
+  "https://www.youtube.com/watch?v=A947JUipDkA&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=99"
+},
+  "101": {
+  "name": "سورہ القارعہ",
+  "link":
+  "https://www.youtube.com/watch?v=2Qw6OuLlpOk&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=100"
+},
+  "102": {
+  "name": "سورہ التکاثر",
+  "link":
+  "https://www.youtube.com/watch?v=cNifNrlIFog&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=101"
+},
+  "103": {
+  "name": "سورہ العصر",
+  "link":
+  "https://www.youtube.com/watch?v=gD3BFfdS0fw&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=102"
+},
+  "104": {
+  "name": "سورہ الہمزہ",
+  "link":
+  "https://www.youtube.com/watch?v=QCw4pc2fsNo&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=103"
+},
+  "105": {
+  "name": "سورہ الفیل",
+  "link":
+  "https://www.youtube.com/watch?v=1_dCslsS7d8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=104"
+},
+  "106": {
+  "name": "قریش",
+  "link":
+  "https://www.youtube.com/watch?v=YEs9pNsi-5s&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=105"
+},
+  "107": {
+  "name": "سورہ الماعون",
+  "link":
+  "https://www.youtube.com/watch?v=2E6VCEY8qmE&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=106"
+},
+  "108": {
+  "name": "سورہ الکوثر",
+  "link":
+  "https://www.youtube.com/watch?v=gk-tOHSn-rQ&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=107"
+},
+  "109": {
+  "name": "سورہ الکافرون",
+  "link":
+  "https://www.youtube.com/watch?v=vheMtf4JYF0&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=108"
+},
+  "110": {
+  "name": "سورہ النصر",
+  "link":
+  "https://www.youtube.com/watch?v=NUUCsgE5ieo&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=109"
+},
+  "111": {
+  "name": "سورہ اللہب",
+  "link":
+  "https://www.youtube.com/watch?v=6z8IFCOfxq8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=110"
+},
+  "112": {
+  "name": "سورہ الاخلاص",
+  "link":
+  "https://www.youtube.com/watch?v=8eXpgH4Nmd4&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=111"
+},
+  "113": {
+  "name": "سورہ الفلق",
+  "link":
+  "https://www.youtube.com/watch?v=EZT35QaC7tQ&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=112"
+},
+  "114": {
+  "name": "سورہ الناس",
+  "link":
+  "https://www.youtube.com/watch?v=AXZHRzHgtq8&list=PLXLJ0Tsrz9yCFDhWVo2Bap9khiOQIdjSA&index=113"
+},
 
 }
