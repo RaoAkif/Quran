40d14f06cf79cdbfc87e806502bbba6a2734e8e1
Rao Akif <akifrao@gmail.com>
2021-04-21 15:29:47 +0500

    Button Text Updated

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
     <div style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}}>
       <h1 style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq", fontSize: '400%'}}>قرآن مجید</h1>
       <button>
-      <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none'}} to="Surahs">تلاوتِ قرآن مجید کے لیے یہاں کلک کریں</Link>
+      <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none'}} to="Surahs">تلاوت کے لیے یہاں کلک کریں</Link>
       </button>
     </div>
   );
