bd12a477df979986933fed4db8a056afeb70fadc
raoakif <akifrao@gmail.com>
2022-08-07 13:02:55 +0500

    Add Complete Splash Screen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
-import splash from '../images/splash.jpg';
+import splash from '../images/splash.png';
 
 export const Home = () => {
   return (
     <div style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}}>
-      <h1 style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq", fontSize: '400%', color: 'white', margin: 'auto', marginTop: '30%'}}>قرآن مجید</h1>
       <div style={{borderRadius: '12px', margin: '0px', padding: '0px', border: '0px'}}>
       <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none', position: 'absolute', top: '56.5%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }} to="Surahs">
       <img style={{width: '100vw', position: 'relative', textAlign: 'center'}} src={splash} alt="splash" />
