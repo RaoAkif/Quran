031cc205dd7e10c8264a31b80c862426382c8015
raoakif <akifrao@gmail.com>
2022-08-07 12:46:09 +0500

    Corrected the styling of Splash

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,8 +11,9 @@ export const Home = () => {
       <br />
       <h1 style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq", fontSize: '400%', color: 'white'}}>قرآن مجید</h1>
       <div style={{borderRadius: '12px', margin: '0px', padding: '0px', border: '0px'}}>
+      <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none', position: 'absolute', top: '56.5%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }} to="Surahs">
       <img style={{width: '100vw', position: 'relative', textAlign: 'center'}} src={splash} alt="splash" />
-      <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none', position: 'absolute', top: '56.5%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }} to="Surahs">داخل ہوں</Link>
+      </Link>
       </div>
     </div>
   );
