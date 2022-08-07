4a3764633d92ac8dfd9fa0c7848bb7ecc842c21a
raoakif <akifrao@gmail.com>
2022-08-07 10:30:42 +0500

    Add Splash Screen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
-// import header from '../images/header.svg';
+import splash from '../images/splash.jpg';
 
 export const Home = () => {
   return (
     <div style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}}>
-      <h1 style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq", fontSize: '400%'}}>قرآن مجید</h1>
-      {/* <img src={header} alt="header" /> */}
-      <button style={{borderRadius: '12px'}}>
-      <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none'}} to="Surahs">تلاوت کے لیے یہاں کلک کریں</Link>
-      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1 style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq", fontSize: '400%', color: 'white'}}>قرآن مجید</h1>
+      <div style={{borderRadius: '12px', margin: '0px', padding: '0px', border: '0px'}}>
+      <img style={{width: '100vw', position: 'relative', textAlign: 'center'}} src={splash} alt="splash" />
+      <Link style={{fontFamily:"Jameel Noori Nastaleeq", fontSize: '200%', textDecorationLine:'none', position: 'absolute', top: '56.5%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }} to="Surahs">داخل ہوں</Link>
+      </div>
     </div>
   );
 };
