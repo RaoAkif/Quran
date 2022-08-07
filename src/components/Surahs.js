40c37ff7340f58963573fa4fe8cbff516db60930
raoakif <akifrao@gmail.com>
2022-08-07 10:03:54 +0500

    Add English name Field in surahData.js

--- src/components/Surahs.js
+++ src/components/Surahs.js
@@ -9,12 +9,15 @@ const Surahs = () => {
     <div style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}}>
       <img style={{width: "100vw"}} src={header} alt="header" />
       <div>
-        {Object.entries(surahData).map(([id, { name }]) => (
+        {Object.entries(surahData).map(([id, { en_name, name }]) => (
           <li style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}} key={id}>
             <Link to={`/Surah/${id}`} style={{ textDecoration: 'none' }}>
               <div className='surah-div'>
-                <p style={{ width:"120px", textDecorationLine:'none', color: 'white', fontSize: '140%', borderRadius: '12px', margin:'20px', fontFamily:"Jameel Noori Nastaleeq", color: "#032D2B" }}>{name}</p>
-                <div style={{ padding: '0px', margin:'0px', color:'#032D2B', textDecorationLine:'none' }}>{id}</div>
+                <p className='surah-en-name' style={{ width:"120px", textDecorationLine:'none', color: 'white', fontSize: '140%', borderRadius: '12px', color: "#032D2B" }}>{en_name}</p>
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                  <p style={{ width:"120px", textDecorationLine:'none', color: 'white', fontSize: '140%', borderRadius: '12px', margin:'20px', fontFamily:"Jameel Noori Nastaleeq", color: "#032D2B" }}>{name}</p>
+                  <div style={{ padding: '0px', margin:'0px', color:'#032D2B', textDecorationLine:'none' }}>{id}</div>
+                </div>
               </div>
             </Link>
           </li>
