da23e482f8367b811fd79b2c98093587587ebe4c
raoakif <akifrao@gmail.com>
2022-08-07 13:08:46 +0500

    Remove English Names of Surah

--- src/components/Surahs.js
+++ src/components/Surahs.js
@@ -14,14 +14,14 @@ const Surahs = () => {
           <li style={{textAlign: 'center', fontFamily:"Jameel Noori Nastaleeq"}} key={id}>
             <Link to={`/Surah/${id}`} style={{ textDecoration: 'none' }}>
               <div className='surah-div'>
-                <p className='surah-en-name' style={{ width:"120px", textDecorationLine:'none', fontSize: '140%', borderRadius: '12px', color: "#032D2B" }}>{en_name}</p>
-                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                  <p style={{ textDecorationLine:'none', fontSize: '140%', borderRadius: '12px', fontFamily:"Jameel Noori Nastaleeq", color: "#032D2B", padding: '0px 10px 0px 0px' }}>{name}</p>
+                {/* <p className='surah-en-name' style={{ width:"120px", textDecorationLine:'none', fontSize: '140%', borderRadius: '12px', color: "#032D2B" }}>{en_name}</p> */}
+                {/* <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}> */}
+                  <p style={{ textDecorationLine:'none', fontSize: '140%', borderRadius: '12px', fontFamily:"Jameel Noori Nastaleeq", color: "#032D2B", padding: '0px 20px 0px 0px' }}>{name}</p>
                   <div class="container">
                     <img style={{width: '70px', marginBottom: '0px'}} src={circleArt} alt="Circle-Art" />
                     <div class="centered" style={{ padding: '0px', margin:'0px', color:'#032D2B', textDecorationLine:'none' }}>{id}</div>
                   </div>
-                </div>
+                {/* </div> */}
               </div>
             </Link>
           </li>
