a12de8ba3d87c129df730de2b586455e1262774d
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-02-20 18:29:55 +0500

    Add clear All checkboxes button functionality

--- src/components/Surahs.js
+++ src/components/Surahs.js
@@ -4,35 +4,43 @@ import "../App.css"
 import "./SurahShowcase.css"
 import header from '../images/header.png';
 import circleArt from '../images/number-circle.png';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const Surahs = () => {
-    // Load stored checkbox values on component mount
-    useEffect(() => {
-      const storedValues = JSON.parse(localStorage.getItem('checkboxes'));
-      if (storedValues) {
-        setCheckboxes(storedValues);
-      }
-    }, []);
-  
-    // Define state for the checkbox values
-    const [checkboxes, setCheckboxes] = useState({});
-  
-    // Update stored values when checkbox values change
-    useEffect(() => {
-      localStorage.setItem('checkboxes', JSON.stringify(checkboxes));
-    }, [checkboxes]);
+  // Load stored checkbox values on component mount
+  useEffect(() => {
+    const storedValues = JSON.parse(localStorage.getItem('checkboxes'));
+    if (storedValues) {
+      setCheckboxes(storedValues);
+    }
+  }, []);
+
+  // Define state for the checkbox values
+  const [checkboxes, setCheckboxes] = useState({});
+
+  // Update stored values when checkbox values change
+  useEffect(() => {
+    localStorage.setItem('checkboxes', JSON.stringify(checkboxes));
+  }, [checkboxes]);
+
+  // Function to handle clearing all checkboxes
+  const handleClearAll = () => {
+    const clearedCheckboxes = {};
+    Object.keys(checkboxes).forEach(id => {
+      clearedCheckboxes[id] = false;
+    });
+    setCheckboxes(clearedCheckboxes);
+  }
 
   return (
     <div style={{ textAlign: 'center', fontFamily: "Jameel Noori Nastaleeq" }}>
       <img style={{ width: "75vw", filter: 'blur(5%)', clipPath: 'inset(1% 1% 1% 1%)' }} src={header} alt="header" />
-      {/* <button style={{ position: 'relative', margin: '0 auto 40px auto', top: '25px', padding: '10px', borderRadius: '5px'}}>Clear All</button> */}
-      <button style={{ display: 'flex', marginLeft: '3vw', padding: '10px', borderRadius: '5px', marginBottom: '10px' }}>Clear All</button>
+      <button style={{ display: 'flex', marginLeft: '3vw', padding: '10px', borderRadius: '5px', marginBottom: '10px' }} onClick={handleClearAll}>Clear All</button>
       <div>
         {Object.entries(surahData).map(([id, { en_name, name }]) => (
           <li style={{ textAlign: 'center', fontFamily: "Jameel Noori Nastaleeq" }} key={id}>
             <div className='surah-div' style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-            <input
+              <input
                 type="checkbox"
                 style={{ width: '20px', height: '20px', color: '#032D2B' }}
                 checked={checkboxes[id]}
