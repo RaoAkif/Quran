0431670ce0c31c9149caad37092166c44acece5b
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-02-20 15:27:08 +0500

    header image side-line removal
    Also decreased header width, which also reduced height

--- src/components/Surahs.js
+++ src/components/Surahs.js
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 import { surahData } from '../surahData';
 import "../App.css"
 import "./SurahShowcase.css"
-import header from '../images/header.PNG';
+import header from '../images/header.png';
 import circleArt from '../images/number-circle.png';
 
 const Surahs = () => {
   return (
     <div style={{ textAlign: 'center', fontFamily: "Jameel Noori Nastaleeq" }}>
-      <img style={{ width: "100vw" }} src={header} alt="header" />
+      <img style={{ width: "75vw" }} src={header} alt="header" />
       <div>
         {Object.entries(surahData).map(([id, { en_name, name }]) => (
           <li style={{ textAlign: 'center', fontFamily: "Jameel Noori Nastaleeq" }} key={id}>
@@ -17,9 +17,9 @@ const Surahs = () => {
               <Link to={`/Surah/${id}`} style={{ textDecoration: 'none' }}>
                 <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                   <p style={{ textDecorationLine: 'none', fontSize: '140%', borderRadius: '12px', fontFamily: "Jameel Noori Nastaleeq", color: "#032D2B", padding: '0px 20px 0px 0px' }}>{name}</p>
-                  <div class="container">
+                  <div className="container">
                     <img style={{ width: '70px', marginBottom: '0px' }} src={circleArt} alt="Circle-Art" />
-                    <div class="centered" style={{ padding: '0px', margin: '0px', color: '#032D2B', textDecorationLine: 'none' }}>{id}</div>
+                    <div className="centered" style={{ padding: '0px', margin: '0px', color: '#032D2B', textDecorationLine: 'none' }}>{id}</div>
                   </div>
                 </div>
               </Link>
