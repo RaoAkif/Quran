02d8bb63318e47b6580fdb7633304f1f0a2892dc
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2026-05-19 20:38:20 +0500

    Add new Icons to the App

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', 'logo192.png', 'logo512.png'],
-  manifest:{
-  name: 'القرآن مجید',
-  short_name: 'القرآن',
-  description:'القرآن مجید - 30 پارے، اردو ترجمے کے ساتھ',
-  icons: [
-    {
-      src: '/favicon.ico',
-      sizes: '48x48',
-      type: 'image/x-icon'
-    },
-    {
-      src: '/logo192.png',
-      type: 'image/png',
-      sizes: '192x192'
-    },
-    {
-      src: '/logo512.png',
-      type: 'image/png',
-      sizes: '512x512'
-    }
-  ],
-  start_url: '/',
-  display: 'standalone',
-  theme_color: '#000000',
-  background_color: '#ffffff'
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', 'logo192.png', 'logo512.png'],
+  manifest: {
+    name: 'القرآن الکریم - عربی مع اردو ترجمہ',
+    short_name: 'القرآن',
+    description: 'القرآن الکریم - 30 پارے، مکمل عربی مع اردو ترجمہ متن',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: '48x48',
+        type: 'image/x-icon'
+      },
+      {
+        src: '/logo192.png',
+        type: 'image/png',
+        sizes: '192x192'
+      },
+      {
+        src: '/logo512.png',
+        type: 'image/png',
+        sizes: '512x512'
+      }
+    ],
+    start_url: '/',
+    display: 'standalone',
+    theme_color: '#000000',
+    background_color: '#ffffff'
   }
 }
 // https://vitejs.dev/config/
